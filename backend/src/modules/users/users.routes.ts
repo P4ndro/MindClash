217447379bd204d39560c9a2ded79d5a29a46012
P4ndro <sandro02050502@gmail.com>
2026-04-09 23:05:42 +0400

    added user controllers. Next connect clerk to DB

--- backend/src/modules/users/users.routes.ts
+++ backend/src/modules/users/users.routes.ts
@@ -1,4 +1,9 @@
 import { Router } from "express";
+import { requireAuth } from "../../middleware/requireAuth";
+import { getMe, getUsers, syncUser } from "./users.controller";
 
 export const usersRouter = Router();
 
+usersRouter.get("/", requireAuth, getUsers);
+usersRouter.post("/sync", requireAuth, syncUser);
+usersRouter.get("/me", requireAuth, getMe);
